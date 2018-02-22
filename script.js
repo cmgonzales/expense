4f4004523a735ad5ea5c1a3991f41eb7ec359b48
Chris Gonzales <cm.gonzales@yahoo.com>
2018-02-21 19:25:57 -0800

    jacascript finished

---
+++ script.js
@@ -0,0 +1,46 @@
+
+document.getElementById("add").addEventListener("click", addexp);
+document.getElementById("calculate").addEventListener("click", add);
+
+var amount = [];
+
+function addexp(){
+var expense = document.getElementById("one").value;
+var cost = document.getElementById("two").value;
+
+var costs = parseInt(cost);
+
+amount.push(costs);
+
+
+console.log(amount);
+
+var items = document.createElement("h4");
+var list = document.createTextNode(expense);
+
+items.appendChild(list);
+
+document.getElementById("item").appendChild(items);
+
+
+var val = document.createElement("h4");
+var dollar = document.createTextNode(cost);
+
+val.appendChild(dollar);
+
+document.getElementById("item").appendChild(val);
+
+}
+
+function add(){
+        var how = 0;
+
+    for (var i = 0; i < amount.length; i++){
+       how +=  amount[i];
+    }
+      
+    console.log(how);
+    
+}
+
+
